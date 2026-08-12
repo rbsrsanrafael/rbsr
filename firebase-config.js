9789f3c938efbdf9af7683782db92817e0bee590
Rural Bank of San Rafael (Bulacan) Inc. <rbsritdepartment@gmail.com>
2026-08-12 18:22:05 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBd9_xtl2tH4HMeuXZdjdLzMoPndnKh2s0",
+  authDomain: "rbsrwebsite-2b69e.firebaseapp.com",
+  projectId: "rbsrwebsite-2b69e",
+  storageBucket: "rbsrwebsite-2b69e.firebasestorage.app",
+  messagingSenderId: "1046190251402",
+  appId: "1:1046190251402:web:32356862d562f9024e3c73",
+  measurementId: "G-81D76QZ61L"
+};
